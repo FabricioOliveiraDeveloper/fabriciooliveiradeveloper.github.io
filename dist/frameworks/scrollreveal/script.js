13a2b83ca4d070e0d38dfa81499532e66b2696ec
Fabricio Oliveira <fabriciooliveirawebdeveloper@gmail.com>
2019-06-13 15:59:13 -0300

    upload

---
+++ dist/frameworks/scrollreveal/script.js
@@ -0,0 +1,38 @@
+ScrollReveal({ duration: 1000 })
+
+ScrollReveal().reveal
+('.animate-top',{ 
+        
+    opacity: '0',
+    distance: '60px',
+    origin: 'top',
+    reset: false,
+    interval: 200  
+
+});
+
+
+ScrollReveal().reveal
+('.animate-fade',{ 
+    
+    opacity: '0',
+    distance: '60px',
+    scale: '0.5',
+    reset: false,
+    interval: 200,
+    delay: 300
+
+});
+
+
+ScrollReveal().reveal
+('.animate-noti-top',{ 
+        
+    opacity: '0',
+    distance: '100px',
+    origin: 'top',
+    reset: false,
+    delay: 5000,
+    interval: 200  
+
+});
